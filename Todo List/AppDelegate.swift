51cbf0bef6049f076f9a69fc25a3c7838f3ccebc
Abdallah Ehab <abdallahehab00@yahoo.com>
2020-05-03 14:39:42 +0200

    persisting data using codable and decodable method

--- Todo List/AppDelegate.swift
+++ Todo List/AppDelegate.swift
@@ -16,8 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        // to get the path 
-print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
+        // to get the path user defult
+  //print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
         return true
         
         // this method call as soon as app is running before everything happen
--- Todo List/controller/TodoListViewcontroler.swift
+++ Todo List/controller/TodoListViewcontroler.swift
@@ -11,33 +11,23 @@ import UIKit
 class TodoListViewcontroler: UITableViewController  {
 
     var itemArray = [Item]()
-    let defults = UserDefaults.standard
+   let dataPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Item.plist")
    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
-        let newItem = Item()
-        newItem.title = "find mike"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "save the world"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "shopping"
-        itemArray.append(newItem3)
+       // print(dataPath)
+       
       
-     
-
-
+    
         
        
         // Do any additional setup after loading the view.
-        if let items = defults.array(forKey: "TodolistArray") as? [Item] {
-            itemArray = items
-        }
+//        if let items = defults.array(forKey: "TodolistArray") as? [Item] { // retriveng data by user defult
+//            itemArray = items
+//        }
+        loadData()
    
     }
     
@@ -56,13 +46,7 @@ class TodoListViewcontroler: UITableViewController  {
         
         // ternary operater  value = condition (?)--> if condition true   set firist value : if false second value
         cell.accessoryType = item.done ? .checkmark : .none
-        
-//        if item.done == true {
-//            cell.accessoryType = .checkmark
-//        }else{
-//            cell.accessoryType = .none
-//
-//        }
+
         return cell
     }
 
@@ -77,8 +61,9 @@ class TodoListViewcontroler: UITableViewController  {
         
     // hatnf3 3shan al reverse etnen value bas ya true ya false f al oppist haynf3 3shan no3hom bool bas  8er keda momken maynf3sh altre2a de
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done  // short way to set oppsit value badl if false = true if true = false
-        
-        tableView.reloadData() // very important to re call method (cellforRow) again and agian fe kol mara ados 3ala item
+        saveData() // just change in done proprety ,cause here you changed inside and change in done baas
+
+       // tableView.reloadData() // very important to re call method (cellforRow) again and agian fe kol mara ados 3ala item
         
         tableView.deselectRow(at: indexPath, animated: true) // after select de select item to look nicer
     }
@@ -96,8 +81,8 @@ class TodoListViewcontroler: UITableViewController  {
             let newItem = Item()
             newItem.title = textfield.text!
             self.itemArray.append(newItem)
-            self.defults.set(self.itemArray, forKey: "TodolistArray")
-            self.tableView.reloadData()
+            self.saveData() // just change in title proprety , see where are you
+            
         }
         alert.addTextField { (alertTextfield) in
             alertTextfield.placeholder = "creat new item"
@@ -106,6 +91,38 @@ class TodoListViewcontroler: UITableViewController  {
          alert.addAction(action)
       
         present(alert, animated: true, completion: nil)
+    }
+    // encoding data
+    func saveData (){
+        let encoder = PropertyListEncoder()
+        do {
+            let  data = try encoder.encode(itemArray) // encode data (itemarray) to plist file
+            try data.write(to: dataPath!) // write data to our file path in plist (writ data in givenlocation)
+
+        }
+        catch {
+            print("error encoding data \(error)")
+        }
+       tableView.reloadData()
+    }
+    
+    // decoding our data
+    func loadData(){
+    
+         // paaset al 3enwan bta3 data law fe 3enwan
+        
+            let decoder = PropertyListDecoder()
+            do {
+                let data = try Data(contentsOf: dataPath!) // if datapath have value nill app will crash so should use optional pinding
+                itemArray = try decoder.decode([Item].self, from: data)
+            
+              }
+            catch {
+                print("error in decoding data \(error)")
+                
+                 }
+                
+        
     }
     
 }
